71bde3b520c17cb3faba6fcdf6cca41571f9771b
gean-lucas <geanfarias@on.br>
2021-11-06 20:05:35 -0300

    Começando interface php

--- bonificacoes.php
+++ bonificacoes.php
@@ -2,26 +2,43 @@
 
 require_once 'autoload.php';
 
+use Alura\Banco\Modelo\Funcionario\Desenvolvedor;
+use Alura\Banco\Modelo\Funcionario\Diretor;
+use Alura\Banco\Modelo\Funcionario\EditorVideo;
+use Alura\Banco\Modelo\Funcionario\Gerente;
 use Alura\Banco\Service\ControladorDeBonificacoes;
 use Alura\Banco\Modelo\Cpf;
-use Alura\Banco\Modelo\Funcionario;
 
-$umFuncionario = new Funcionario(
+$umFuncionario = new Desenvolvedor(
     'Gean Lucas', 
-    new Cpf('123.456.789-10'), 
-    'Desenvolvedor', 
+    new Cpf('123.456.789-10'),
     1000.0
 );
 
-$umaFuncionaria = new Funcionario(
-    'ishdaisjfh',
+$umFuncionario->sobeDeNivel();
+
+$umaFuncionaria = new Gerente(
+    'Liziane',
     new Cpf('987.65.321-10'),
-    'Desenvolvedor',
     3000.0
+);
+
+$umDiretor = new Diretor(
+    'Ana Maria',
+    new Cpf('123.654.789-10'),
+    5000
+);
+
+$umEditor = new EditorVideo(
+    'Paulo',
+    new Cpf('456789123-10'),
+    1500
 );
 
 $controlador = new ControladorDeBonificacoes();
 $controlador->adicionaBonificacaoDe($umFuncionario);
 $controlador->adicionaBonificacaoDe($umaFuncionaria);
+$controlador->adicionaBonificacaoDe($umDiretor);
+$controlador->adicionaBonificacaoDe($umEditor);
 
 echo $controlador->recuperaTotal();
---
+++ src/Service/Autenticador.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Alura\Banco\Service;
+
+class Autenticador
+{
+    public function tentaLogin(Autenticavel $autenticavel, string $senha): void
+    {
+        if ($autenticavel->podeAutenticar($senha)) {
+            echo 'Ok. Usuário logado no sistema.';
+        } else {
+            echo 'Ops. Senha incorreta.';
+        }
+    }
+}
